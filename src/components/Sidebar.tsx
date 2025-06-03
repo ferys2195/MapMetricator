edf582653451b675732edfa28a70325121a13622
Fery on Kantor <ferys2195@gmail.com>
2025-06-03 12:29:15 +0700

    perbaikan style marker list pada sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,11 +7,13 @@ import {
 import GPXParser from "./UploadGPX";
 import { useMarkerStore } from "@/stores/useMarkerStore";
 import { latLngToUtm } from "@/lib/geoUtils";
+import { Trash2 } from "lucide-react";
+import { Button } from "./ui/button";
 
 const Sidebar = () => {
-  const { markers } = useMarkerStore();
+  const { removeMarker, markers } = useMarkerStore();
   return (
-    <div className="relative h-full space-y-4 border-r bg-white p-4 text-sm">
+    <div className="relative h-full space-y-4 overflow-y-auto border-r bg-white p-4 text-sm">
       <h2 className="text-lg font-semibold">Marker List</h2>
 
       <Accordion
@@ -37,13 +39,25 @@ const Sidebar = () => {
         <AccordionItem value="marker" data-state="open">
           <AccordionTrigger>Marker List</AccordionTrigger>
           <AccordionContent>
-            <ol className="list-decimal">
-              {markers.map((marker) => (
-                <li>
-                  <code>{latLngToUtm(marker).getAsString}</code>
+            <ul>
+              {markers.map((marker, index) => (
+                <li key={index}>
+                  <div className="flex items-center justify-between">
+                    <code>
+                      {String(++index).padStart(3, "0")}{" "}
+                      {latLngToUtm(marker).getAsString}
+                    </code>
+                    <Button
+                      variant={"ghost"}
+                      size={"icon"}
+                      onClick={() => removeMarker(marker)}
+                    >
+                      <Trash2 className="text-destructive" />
+                    </Button>
+                  </div>
                 </li>
               ))}
-            </ol>
+            </ul>
           </AccordionContent>
         </AccordionItem>
       </Accordion>
