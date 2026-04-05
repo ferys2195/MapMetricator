b9e5a785367f8d8972d3568fa905b172c6edc416
ferys2195 <fery21irawan.me@gmail.com>
2026-04-05 09:16:08 +0700

    refactor : restructure project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import "./App.css";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { useIsMobile } from "./hooks/use-mobile";
 import MapPage from "./pages/MapPage";
 
 function App() {
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return (
+      <div className="flex h-screen items-center justify-center p-4">
+        <p className="text-muted-foreground text-center text-lg">
+          Website is not supported on mobile
+        </p>
+      </div>
+    );
+  }
   return (
-    <>
+    <TooltipProvider>
       <MapPage />
-    </>
+    </TooltipProvider>
   );
 }
 
