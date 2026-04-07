2e565e42f830e14ea91cbb47b3297323b603c450
ferys2195 <fery21irawan.me@gmail.com>
2026-04-07 21:54:04 +0700

    feat : restructure menu

--- src/components/MeasureMenu.tsx
+++ src/components/MeasureMenu.tsx
@@ -1,39 +1,16 @@
-import { LandPlot, MapPinPlus, Ruler } from "lucide-react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "./ui/dialog";
+import { LandPlot, MapPinPlus, Waypoints } from "lucide-react";
+
 import { Button } from "./ui/button";
-import { useMarkerStore } from "@/stores/useMarkerStore";
 import { useEffect, useState } from "react";
 import { useMap } from "react-leaflet";
-import { latLngToUtm } from "@/lib/geoUtils";
-import { Checkbox } from "./ui/checkbox";
-import { Label } from "@radix-ui/react-dropdown-menu";
-import { usePolygonStore } from "@/stores/polygonStore";
+
 import { ButtonGroup } from "./ui/button-group";
 import { useMapModeStore } from "@/stores/useMapModeStore";
 
 export default function MeasureMenu() {
-  const { markers } = useMarkerStore();
   const { mode, setMode } = useMapModeStore();
   const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
-  const [selectedMarker, setSelectedMarker] = useState<
-    { lat: number; lng: number }[]
-  >([]);
   const map = useMap();
-
-  const { addCoord } = usePolygonStore();
-
-  const addCoordinate = () => {
-    selectedMarker.map((marker) => {
-      addCoord([marker.lng, marker.lat]);
-    });
-  };
 
   useEffect(() => {
     const container = map.getContainer();
@@ -45,7 +22,7 @@ export default function MeasureMenu() {
       });
     }
 
-    if (mode === "marker") {
+    if (mode !== "idle") {
       setCursor("crosshair");
     } else {
       setCursor("");
@@ -78,74 +55,10 @@ export default function MeasureMenu() {
         aria-label="Measure Menu"
         className="h-fit"
       >
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button size={"icon"} title="Ukur Bidang" variant={"secondary"}>
-              <LandPlot size={16} />
-            </Button>
-          </DialogTrigger>
-          <DialogContent
-            className="z-[9999] sm:max-w-[425px]"
-            onInteractOutside={(e) => e.preventDefault()}
-            onEscapeKeyDown={(e) => e.preventDefault()}
-          >
-            <DialogHeader>
-              <DialogTitle>Ukur Bidang</DialogTitle>
-              <DialogDescription>
-                Mengukur bidang berdasarkan koordinat. pilih minimal 3 koordinat
-              </DialogDescription>
-            </DialogHeader>
-            <ul className="mb-5">
-              {markers.map((marker, index) => (
-                <li key={index}>
-                  <div className="flex items-center gap-3">
-                    <Checkbox
-                      id={String(index)}
-                      key={index}
-                      onCheckedChange={(checked) => {
-                        if (checked) {
-                          // Tambahkan marker ke array jika checkbox dicentang
-                          setSelectedMarker((prev) => [
-                            ...prev,
-                            { lat: marker.lat, lng: marker.lng },
-                          ]);
-                        } else {
-                          // Hapus marker dari array jika checkbox tidak dicentang
-                          setSelectedMarker((prev) =>
-                            prev.filter(
-                              (m) =>
-                                !(m.lat === marker.lat && m.lng === marker.lng),
-                            ),
-                          );
-                        }
-                      }}
-                    />
-                    <Label>
-                      <code>
-                        {String(++index).padStart(3, "0")}{" "}
-                        {latLngToUtm(marker).getAsString}
-                      </code>
-                    </Label>
-                  </div>
-                </li>
-              ))}
-            </ul>
-            <Button
-              className="w-full"
-              disabled={selectedMarker.length < 3 ? true : false}
-              onClick={() => {
-                console.log(selectedMarker);
-                addCoordinate();
-              }}
-            >
-              Submit
-            </Button>
-          </DialogContent>
-        </Dialog>
         <Button
           variant={mode === "marker" ? "default" : "secondary"}
           size="icon"
-          title="Tambah Marker"
+          title="Add Marker"
           onClick={(e) => {
             e.stopPropagation();
             setMode(mode === "marker" ? "idle" : "marker");
@@ -162,7 +75,18 @@ export default function MeasureMenu() {
             setMode(mode === "polyline" ? "idle" : "polyline");
           }}
         >
-          <Ruler />
+          <Waypoints />
+        </Button>
+        <Button
+          variant={mode === "polygone" ? "default" : "secondary"}
+          size="icon"
+          title="Draw Area"
+          onClick={(e) => {
+            e.stopPropagation();
+            setMode(mode === "polygone" ? "idle" : "polygone");
+          }}
+        >
+          <LandPlot />
         </Button>
       </ButtonGroup>
 
